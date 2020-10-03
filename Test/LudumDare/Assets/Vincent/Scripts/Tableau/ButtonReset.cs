c0d9f851c53adce6d968fa3ae06fc7bb00e3697e
VincentB2 <hjklm96301@gmail.com>
2020-10-03 19:48:48 +0200

    Petit push de 20h

---
+++ Test/LudumDare/Assets/Vincent/Scripts/Tableau/ButtonReset.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class ButtonReset : MonoBehaviour
+{
+    public GameObject case1;
+    public GameObject case2;
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(case1.transform.childCount != 0)
+            {
+                Transform ressource = case1.transform.GetChild(0);
+                ressource.parent = null;
+                ressource.GetComponent<DragAndDrop>().ResetPosition();
+            }
+            if (case2.transform.childCount != 0)
+            {
+                Transform ressource = case2.transform.GetChild(0);
+                ressource.parent = null;
+                ressource.GetComponent<DragAndDrop>().ResetPosition();
+            }
+        }
+    }
+
+}
