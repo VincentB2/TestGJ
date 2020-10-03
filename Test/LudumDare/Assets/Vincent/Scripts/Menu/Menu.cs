a722c49b0e4f61d459de84266c9601aaf4c5cfdc
VincentB2 <hjklm96301@gmail.com>
2020-10-03 10:05:15 +0200

    Menu setup

---
+++ Test/LudumDare/Assets/Vincent/Scripts/Menu/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options", LoadSceneMode.Single);
+    }
+
+    public void Exit()
+    {
+
+        Application.Quit();
+
+    }
+
+}
