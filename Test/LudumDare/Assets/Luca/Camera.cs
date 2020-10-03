9654d70cc5d32d7f197ada481b65529a47f42101
Storm <luca.schifano005@gmail.com>
2020-10-03 10:52:21 +0200

    package + camera

---
+++ Test/LudumDare/Assets/Luca/Camera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    public float leftLimit;
+    public float rightLimit;
+    public float bottomLimit;
+    public float upLimit;
+
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        
+    }
+
+    void Update(){
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, upLimit),
+            transform.position.z
+        );
+        
+    }
+}
