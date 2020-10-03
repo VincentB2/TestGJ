11149e5fac1f3f66982d9fd5bd926d8c6e1cee36
Storm <luca.schifano005@gmail.com>
2020-10-03 12:04:38 +0200

    Code + GeneratorCode + DetectPlayer

---
+++ Test/LudumDare/Assets/Luca/Script/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public int genCode0;    
+    public int genCode1;    
+    public int genCode2;    
+    public int genCode3;
+
+    void Start()
+    {
+        var randomPaper = GameObject.Find("RandomCode0").GetComponent<RandomPaper>();
+        genCode0 = randomPaper.GeneratorCode0;
+        genCode1 = randomPaper.GeneratorCode1;
+        genCode2 = randomPaper.GeneratorCode2;
+        genCode3 = randomPaper.GeneratorCode3;
+
+        Debug.Log(genCode0);
+        Debug.Log(genCode1);
+        Debug.Log(genCode2);
+        Debug.Log(genCode3);
+
+    }
+
+    void Update()
+    {
+        var PIH = GameObject.Find("Generator").GetComponent<PlayerIsHere>();
+        if(PIH.isPlayer){
+            if(Input.GetKeyDown("e")){
+                Debug.Log("coucou");
+            }
+        }
+    }
+}
---
+++ Test/LudumDare/Assets/Luca/Script/RandomPaper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPaper : MonoBehaviour
+{
+
+    public int GeneratorCode0;
+    public int GeneratorCode1;
+    public int GeneratorCode2;
+    public int GeneratorCode3;
+    void Start()
+    {
+        /*GeneratorCode0 = Random.Range(0, 10);
+        GeneratorCode1 = Random.Range(0, 10);
+        GeneratorCode2 = Random.Range(0, 10);
+        GeneratorCode3 = Random.Range(0, 10);*/
+        
+        /*Debug.Log(GeneratorCode0);
+        Debug.Log(GeneratorCode1);
+        Debug.Log(GeneratorCode2);
+        Debug.Log(GeneratorCode3);*/
+    }
+
+    void Update()
+    {
+    }
+}
