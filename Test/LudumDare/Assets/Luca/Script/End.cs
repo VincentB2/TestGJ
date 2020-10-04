7c5fc23951d94023db08e6b1b6d9531b79c4fabe
Storm <luca.schifano005@gmail.com>
2020-10-04 17:36:54 +0200

    Menu Credit Controls

---
+++ Test/LudumDare/Assets/Luca/Script/End.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+         var PIH = GameObject.Find("Portal").GetComponent<PlayerIsHere>();
+        var player = GameObject.Find("Player").GetComponent<Player>();
+
+        if(PIH.isPlayer){
+            if(Input.GetKeyDown("e")){
+                SceneManager.LoadScene("Luca", LoadSceneMode.Single);
+            }
+        }
+    }
+}
--- Test/LudumDare/Assets/Vincent/Scripts/Menu/Menu.cs
+++ Test/LudumDare/Assets/Vincent/Scripts/Menu/Menu.cs
@@ -10,12 +10,22 @@ public class Menu : MonoBehaviour
 
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+        SceneManager.LoadScene("Luca", LoadSceneMode.Single);
     }
 
     public void Options()
     {
         SceneManager.LoadScene("Options", LoadSceneMode.Single);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits", LoadSceneMode.Single);
+    }
+
+    public void returnToMenu(){
+        SceneManager.LoadScene("Menu 1", LoadSceneMode.Single);
     }
 
     public void Exit()
