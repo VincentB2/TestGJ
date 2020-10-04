15842ec51ae55a807d421be9006877f21e316466
Storm <luca.schifano005@gmail.com>
2020-10-04 19:00:38 +0200

    Insertion audio manager

---
+++ Test/LudumDare/Assets/Luca/Script/Sound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public new string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
