3856ac90bb77e16938983517547062741abe8668
Storm <luca.schifano005@gmail.com>
2020-10-04 12:49:10 +0200

    Fusible activable que avec le generateur + Carton et porte fusible + Hint press "e" + Mini inventaire

---
+++ Test/LudumDare/Assets/Luca/Script/InteractionHint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionHint : MonoBehaviour
+{
+    public bool isInteraction;
+    public GameObject text;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(isInteraction){
+            text.SetActive(true);
+        }   else{
+            text.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col){
+        if (col.gameObject.tag == "Interaction"){ 
+            isInteraction = true;
+
+        }
+    }
+
+     void OnTriggerExit2D(Collider2D col){
+         if (col.gameObject.tag == "Interaction"){ 
+            isInteraction = false;
+
+        }
+     }
+}
