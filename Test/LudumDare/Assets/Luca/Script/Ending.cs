e4c32ef598049ec86d95f10b2bccf74d188699ce
Storm <luca.schifano005@gmail.com>
2020-10-05 12:19:00 +0200

    Scene Fix + Ending

---
+++ Test/LudumDare/Assets/Luca/Script/Ending.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine("end");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator end(){
+        yield return new WaitForSeconds(4.05f);
+        SceneManager.LoadScene("Luca", LoadSceneMode.Single);
+    }
+}
