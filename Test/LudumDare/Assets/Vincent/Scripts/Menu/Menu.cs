55367e6e35cf355da1ea14da53da6870d37d2d68
Storm <luca.schifano005@gmail.com>
2020-10-05 10:22:44 +0200

    Menu Sound

--- Test/LudumDare/Assets/Vincent/Scripts/Menu/Menu.cs
+++ Test/LudumDare/Assets/Vincent/Scripts/Menu/Menu.cs
@@ -11,28 +11,57 @@ public class Menu : MonoBehaviour
     public void Play()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+        FindObjectOfType<AudioManager>().Play("Click");
+        StartCoroutine("play2");
+    }
+
+    IEnumerator play2(){
+        yield return new WaitForSeconds(0.2f);
         SceneManager.LoadScene("Luca", LoadSceneMode.Single);
     }
 
     public void Options()
     {
+        FindObjectOfType<AudioManager>().Play("Click");
+        StartCoroutine("opt2");
+    }
+
+    IEnumerator opt2(){
+        yield return new WaitForSeconds(0.2f);
         SceneManager.LoadScene("Options", LoadSceneMode.Single);
     }
 
     public void Credits()
     {
+        FindObjectOfType<AudioManager>().Play("Click");
+        StartCoroutine("cred2");
+    }
+
+    IEnumerator cred2(){
+        yield return new WaitForSeconds(0.2f);
         SceneManager.LoadScene("Credits", LoadSceneMode.Single);
     }
 
-    public void returnToMenu(){
+    public void returnToMenu(){     
+        FindObjectOfType<AudioManager>().Play("Click");
+        StartCoroutine("rtm2");
+    }
+
+    IEnumerator rtm2(){
+        yield return new WaitForSeconds(0.2f);
         SceneManager.LoadScene("Menu 1", LoadSceneMode.Single);
     }
 
     public void Exit()
     {
+        FindObjectOfType<AudioManager>().Play("Click");
+        StartCoroutine("exit2");
 
+    }
+
+    IEnumerator exit2(){
+        yield return new WaitForSeconds(0.2f);
         Application.Quit();
-
     }
 
 }
